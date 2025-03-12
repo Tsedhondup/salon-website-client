f81ba247c66a52fa61bc3e9530d78420ca1fe0c5
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-12 18:03:49 -0400

    HomePageService component created and imported

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import HomePageServices from "../../components/homePageServices/homePageServices";
 const HomePage = () => {
   return (
     <section>
       <Header />
       <Hero />
+      <HomePageServices />
     </section>
   );
 };
