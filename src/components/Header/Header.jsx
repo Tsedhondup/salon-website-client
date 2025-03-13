744b594eac6d7067b95ae7b4bce7118f57f8808d
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-12 21:05:04 -0400

    class names added to header content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,26 @@ const Header = () => {
   return (
     <section className="header">
       {/* Before scroll up */}
-      <section>
-        {/* Salon history  */}
-        <h2>since 2022</h2>
-        {/* Salon Location */}
-        <div>1530 Albion Rd, Etobicoke, ON M9V 1B4</div>
+      <section className="header__before-scroll">
+        <h2 className="header__birth-year">since 2022</h2>
+        <h2 className="header_address-top">
+          1530 Albion Rd, Etobicoke, ON M9V 1B4
+        </h2>
       </section>
       {/* After scroll up */}
-      <section>
-        <div>
-          <h3>Nano's Beauty Salon</h3>
-          <div>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>
-            <a href="#">About Us</a>
+      <section className="header__after-scroll">
+        <div className="header-links">
+          <h3 className="header-links__merchant-name">Nano's Beauty Salon</h3>
+          <div className="heade-links__link-container">
+            <a href="#" className="header__link-container--link">
+              Services
+            </a>
+            <a href="#" className="header__link-container--link">
+              Contact
+            </a>
+            <a href="#" className="header__link-container--link">
+              About Us
+            </a>
           </div>
         </div>
       </section>
