7522c58b520d8a7d6d5622e763a4944c1ccd4e30
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-15 00:29:57 -0400

    typo error fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
-import HomePageServices from "../../components/homePageServices/homePageServices";
+import HomePageServices from "../../components/HomePageServices/HomePageServices";
 import Gallery from "../../components/Gallery/Gallery";
 import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
