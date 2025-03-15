b9fa8b31ccb380024a5bd0d2d0b1940aabf7516f
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-15 15:22:38 -0400

    class names added to HomePageService component

--- src/components/HomePageServices/HomePageServices.jsx
+++ src/components/HomePageServices/HomePageServices.jsx
@@ -1,39 +1,24 @@
 // import services from "../../data/services.json";
+import nanoClientImage from "../../assets/images/nano1.jpg";
 const HomePageServices = () => {
   return (
-    <section>
-      <section>
-        <div>
-          <img />
-          <h3>title </h3>
-          <p>This is description</p>
-        </div>
-        <div>
-          <img />
-          <h3>title </h3>
-          <p>This is description</p>
-        </div>
-        <div>
-          <img />
-          <h3>title </h3>
-          <p>This is description</p>
-        </div>
-        <div>
-          <img />
-          <h3>title </h3>
-          <p>This is description</p>
-        </div>
-        <div>
-          <img />
-          <h3>title </h3>
-          <p>This is description</p>
+    <section className="service-home-page">
+      <section className="service-home-page__content">
+        <div className="image-text-container">
+          <img
+            className="image-text-container"
+            src={nanoClientImage}
+            alt="image from salon"
+          />
+
+          <div className="image-text-container__texts">
+            <h3 className="image-text-container__texts--header">48+ </h3>
+            <p className="image-text-container__texts--body">services by</p>
+            <h3 className="image-text-container__texts--header">10 </h3>
+            <p className="image-text-container__texts--body">Expert stylists</p>
+          </div>
         </div>
       </section>
-      {/* See more and Book now button */}
-      <div>
-        <button>Book Now</button>
-        <button>See more</button>
-      </div>
     </section>
   );
 };
