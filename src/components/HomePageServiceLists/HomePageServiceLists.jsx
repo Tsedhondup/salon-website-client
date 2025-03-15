8d06b9133fbf85ebde192eb22640f768f03f842f
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-15 17:56:01 -0400

    images added

---
+++ src/components/HomePageServiceLists/HomePageServiceLists.jsx
@@ -0,0 +1,19 @@
+const HomePageServiceLists = () => {
+  return (
+    <section>
+      <div>
+        <h2>Waxing</h2>
+        <p></p>
+      </div>
+      <div>
+        <h2>Hair</h2>
+        <p></p>
+      </div>
+      <div>
+        <h2>Bear</h2>
+        <p></p>
+      </div>
+    </section>
+  );
+};
+export default HomePageServiceLists;
