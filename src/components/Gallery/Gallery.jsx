d1a6ab244fd69545d9a3d3dae2e424cab9a6a6bc
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-16 19:03:59 -0400

    Image imported to gallery component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,7 +1,14 @@
+import "./Gallery.scss";
+import image1 from "../../assets/images/g1.jpeg";
+import image2 from "../../assets/images/g2.jpeg";
+import image3 from "../../assets/images/g3.jpeg";
+import image4 from "../../assets/images/client1.jpeg";
+
 const Gallery = () => {
+  const images = [image1, image2, image3, image4];
   return (
     <section>
-      <h2>Gallery</h2>
+      <h2>Nano's Gallery</h2>
       <div>
         <img />
         <img />
@@ -15,4 +22,4 @@ const Gallery = () => {
     </section>
   );
 };
-export default Gallery
+export default Gallery;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
       <HomePageServices />
       <HomePageServiceLists />
       <Testimonials />
-      {/* <Gallery /> */}
+      <Gallery />
       <Contact />
       <Footer />
     </section>
