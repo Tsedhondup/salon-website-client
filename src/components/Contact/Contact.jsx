61168be9c90ee9f9cdd9b5680205ea1961d77d2f
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-15 23:10:58 -0400

    class name added to Contact content

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,13 @@
+import "./Contact.scss";
 const Contact = () => {
   return (
-    <section>
+    <section className="contacts">
+      <p className="contacts__booking-info">
+        For reservation and any other inquiries, Please call us directly at
+        <a className="contacts__booking-info--phone-number">+416-744-0019</a>
+      </p>
       <div>
         <h2>Get in touch </h2>
-        <p>
-          For reservation and any other inquiries, Please call us directly at
-          <a>+416-744-0019</a>
-        </p>
       </div>
       <div>This will be replace relavent image </div>
     </section>
