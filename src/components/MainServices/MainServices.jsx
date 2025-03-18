80444bcf44c33cd1e4439af614ca5e29f6639689
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-18 12:24:25 -0400

    animation for images completed

--- src/components/MainServices/MainServices.jsx
+++ src/components/MainServices/MainServices.jsx
@@ -5,9 +5,35 @@ import hair from "../../assets/images/hair-service.jpg";
 import beard from "../../assets/images/beard-service.jpg";
 import brow from "../../assets/images/brow-service.jpg";
 import facial from "../../assets/images/facial-service.jpg";
+import { motion } from "framer-motion";
 
 import "./MainServices.scss";
 const MainServices = () => {
+  const chooseXcoordinate = (serviceName) => {
+    if (
+      serviceName === "Hair" ||
+      serviceName === "Brow" ||
+      serviceName === "Waxing"
+    ) {
+      return -60;
+    } else {
+      return 60;
+    }
+  };
+
+  // Animation
+  const initialView = {
+    opacity: 0,
+    y: 60,
+  };
+  const whileInView = {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: "linear",
+      duration: 0.5,
+    },
+  };
   const images = [hair, lash, brow, beard, waxing, facial];
   let indexNumber = 0;
   const serviceData = services.map((item) => {
@@ -15,33 +41,63 @@ const MainServices = () => {
   });
   return (
     <section className="main-services">
-      <h1 className="main-services__header">
-        Discover a wide range of services at{" "}
+      <motion.h1
+        className="main-services__header"
+        initial={initialView}
+        whileInView={whileInView}
+        viewport={{ once: true, amount: 1 }}
+      >
+        Discover a wide range of services at
         <span className="main-services__header--salon-name">
           Nano Beauty Salon
         </span>
         , tailored to elevate your beauty experience
-      </h1>
+      </motion.h1>
 
       <div className="main-services__lists">
         {serviceData.map((item) => {
           const serviecEl = (
             <div className="main-services__list-item">
-              <div className={`main-services__${item.category}-container`}>
-                <img
+              <motion.div
+                className={`main-services__${item.category}-container`}
+              >
+                <motion.img
                   src={images[indexNumber]}
                   alt={item.category}
-                  className="main-services__list-item--image"
+                  className={`
+                  main-services__list-item--image 
+                  main-services__list-item--${item.category}-image
+                  `}
+                  initial={{
+                    opacity: 0,
+                    // x: chooseXcoordinate(item.category),
+                    // translateX: 60,
+                    translateX: chooseXcoordinate(item.category),
+                  }}
+                  whileInView={{
+                    opacity: 1,
+                    translateX: 0,
+                  }}
+                  transition={{
+                    ease: "linear",
+                    duration: 0.5,
+                  }}
+                  viewport={{ once: true, amount: 0 }}
                 />
-              </div>
+              </motion.div>
 
               <div
                 key={item.category}
                 className={`main-services__list-item--texts main-services__list-item--${item.category}-texts`}
               >
-                <h2 className="main-services__list-item--category">
+                <motion.h2
+                  className="main-services__list-item--category"
+                  initial={initialView}
+                  whileInView={whileInView}
+                  viewport={{ once: true, amount: 1 }}
+                >
                   {item.category}
-                </h2>
+                </motion.h2>
                 <div className="main-services__service-detail-container">
                   {item.services.map((item2) => {
                     // Individual service and its detail
@@ -50,15 +106,30 @@ const MainServices = () => {
                         key={item2.name}
                         className="main-services__service-detail-content"
                       >
-                        <h3 className="main-services__service-detail-content--name">
+                        <motion.h3
+                          className="main-services__service-detail-content--name"
+                          initial={initialView}
+                          whileInView={whileInView}
+                          viewport={{ once: true, amount: 1 }}
+                        >
                           {item2.name}
-                        </h3>
-                        <h3 className="main-services__service-detail-content--price">
+                        </motion.h3>
+                        <motion.h3
+                          className="main-services__service-detail-content--price"
+                          initial={initialView}
+                          whileInView={whileInView}
+                          viewport={{ once: true, amount: 1 }}
+                        >
                           {item2.price}
-                        </h3>
-                        <h3 className="main-services__service-detail-content--duration">
+                        </motion.h3>
+                        <motion.h3
+                          className="main-services__service-detail-content--duration"
+                          initial={initialView}
+                          whileInView={whileInView}
+                          viewport={{ once: true, amount: 1 }}
+                        >
                           {item2.duration}
-                        </h3>
+                        </motion.h3>
                       </div>
                     );
                   })}
--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import MainServices from "../../components/mainServices/MainServices";
+import Gallery from "../../components/Gallery/Gallery";
 import Footer from "../../components/Footer/Footer";
 const ServicePage = () => {
   return (
     <>
       <Header />
       <MainServices />
+      <Gallery />
       <Footer />
     </>
   );
