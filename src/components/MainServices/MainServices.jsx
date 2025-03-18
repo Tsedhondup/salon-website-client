37c723d7970433cfeea2b0c06f1ad22ef24c108d
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-17 23:03:58 -0400

    image added

--- src/components/MainServices/MainServices.jsx
+++ src/components/MainServices/MainServices.jsx
@@ -1,3 +1,14 @@
+import services from "../../data/services.json";
+
 import "./MainServices.scss";
-const MainServices = () => {};
+const MainServices = () => {
+  return (
+    <section className="main-services">
+      <h1 className="main-services__headline">
+        Discover a wide range of services at Nano Beauty Salon, tailored to
+        elevate your beauty experience
+      </h1>
+    </section>
+  );
+};
 export default MainServices;
--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,3 +1,4 @@
+
 import Header from "../../components/Header/Header";
 import MainServices from "../../components/mainServices/MainServices";
 const ServicePage = () => {
