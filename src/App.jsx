97aa208752aacafd684563d910db6fbf9448fd56
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-17 22:30:11 -0400

    react-router-dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/About" element={<AboutPage />} /> */}
+        </Routes>
+        {/* <Footers /> */}
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
