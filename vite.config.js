eb990dc5329e581d720ec8abcb90cd7cee033c64
Tsering Dhondup <tyddhondup88@gmail.com>
2025-03-20 22:41:31 -0400

    configure server

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.woff", "**/*.woff2"], // Ensure Vite recognizes font files
+  server: {
+    historyApiFallback: true, // Ensures React handles routing
+  },
 });
